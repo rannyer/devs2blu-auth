3d36c88a5a53eebd3c7612ab1a36358e6b9ef0c7
Rannyer <rdesenhos@gmail.com>
2026-03-20 21:26:05 +0100

    testes adicionandos

--- src/test/java/com/project/auth_service/demo/cucumber/AuthSteps.java
+++ src/test/java/com/project/auth_service/demo/cucumber/AuthSteps.java
@@ -5,6 +5,9 @@ import com.project.auth_service.demo.models.dtos.LoginResponseDTO;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.hu.Ha;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class AuthSteps {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthSteps.class);
     @Autowired
     private MockMvc mockMvc;
 
@@ -30,6 +34,7 @@ public class AuthSteps {
 
     private Map<String, Object> requestBody;
     private MvcResult response;
+    private Map<String, Object> loginBody;
 
     @Given("a user exists with login {string} and password {string} and role {string}")
     public void aUserExistsWithLoginAndPassword(String login, String password, String role) throws Exception{
@@ -51,6 +56,34 @@ public class AuthSteps {
                 .contentType("application/json")
                 .content(objectMapper.writeValueAsString(requestBody)))
             .andReturn();
+    }
+    @When("the user sends a POST request to {string} with the credentials {string} and password {string}")
+    public void theUserSendsAPostRequestWIthLoginAndPassword(String endpoint, String login, String password) throws Exception {
+        loginBody = new HashMap<>();
+        loginBody.put("login", login);
+        loginBody.put("password", password);
+
+        response = mockMvc.perform(post(endpoint)
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(loginBody)))
+                .andReturn();
+    }
+
+    @When("a user sends a POST request to {string} with login {string}, password {string}, and role {string}")
+    public void registerWithInvalidRole(String endpoint, String login, String password, String role) throws Exception {
+        Map<String, Object> body = new HashMap<>();
+        body.put("login", login);
+        body.put("password", password);
+        body.put("role",role);
+
+        response = mockMvc.perform(post(endpoint)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(body)))
+                .andReturn();
+
+        System.out.println("REGISTER STATUS: " + response.getResponse().getStatus());
+        System.out.println("REGISTER BODY: "+response.getResponse().getContentAsString());
+
     }
 
     @Then("the response status code should be {int}")
--- src/test/resources/features/auth.feature
+++ src/test/resources/features/auth.feature
@@ -5,5 +5,11 @@ Feature:Authentication
     When the user sends a POST request to "/auth/login" with the credentials
     Then the response status code should be 200
 
+  Scenario: Login with invalid password
+    Given a user exists with login "vet13" and password "234" and role "VET"
+    When the user sends a POST request to "/auth/login" with the credentials "vet13" and password "999"
+    Then the response status code should be 403
 
-
+  Scenario: Register with invalid role
+    When a user sends a POST request to "/auth/register" with login "vet14", password "123", and role "INVALID_ROLE"
+    Then the response status code should be 400
