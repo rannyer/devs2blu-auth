d21819f87f81ae7eef933e27780ab402becec8a8
Rannyer <rdesenhos@gmail.com>
2026-05-13 18:46:08 -0300

    testes adicionandos

--- src/test/java/com/project/auth_service/demo/controller/AuthControllerTest.java
+++ src/test/java/com/project/auth_service/demo/controller/AuthControllerTest.java
@@ -1,8 +1,10 @@
 package com.project.auth_service.demo.controller;
 
+import com.project.auth_service.demo.repositories.UserRepository;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -12,17 +14,39 @@ public class AuthControllerTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private UserRepository repository;
+
     @BeforeEach
     void setUp() {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = port;
+
+        repository.deleteAll();
     }
 
     @Test
     void deveAcessarRotaProtegidaComToken(){
+
+        String registerPayload = """
+                {
+                    "login": "javeteri",
+                    "password": "123",
+                    "role": "VET"
+                }
+                """;
+        RestAssured
+                .given()
+                    .body(registerPayload)
+                    .contentType("application/json")
+                .when()
+                    .post("/auth/register")
+                .then()
+                    .statusCode(200);
+
         String payloadLogin = """
                    {
-                    "login": "javet",
+                    "login": "javeteri",
                     "password": "123"
                    }
                 """;
